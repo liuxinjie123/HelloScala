edc185dff3ea3a812a91b8d35f808612d3f796aa
liuxinjie123 <jackcodejs@gmail.com>
2018-06-17 23:32:00 +0800

    fun

--- src/main/com/hello/Fun.scala
+++ src/main/com/hello/Fun.scala
@@ -6,7 +6,54 @@ object Fun {
     * @param x
     * @return |x|
     */
-  def abs(x: Double) = if (x > 0) x else -x;
+  def abs(x: Double) = if (x > 0) x else -x
 
+  /**
+    * 阶乘
+    * @param n
+    * @return n的阶乘值
+    */
+  def fac(n: Int) = {
+    var r = 1
+    for (i <- 1 to n) r = r * i
+    r
+  }
+
+  /**
+    * 阶乘2
+    * @param n
+    * @return n的阶乘值
+    */
+  def fac2(n: Int): Int = if (n <= 1) 1 else n * fac2(n-1)
+
+  /**
+    * 封装 字符串
+    * @param str    字符串
+    * @param left   字符串左边字符
+    * @param right  字符串右边字符
+    * @return       封装好的字符串
+    */
+  def decorate(str: String, left: String = "[", right: String = "]") = left + str + right
+
+  /**
+    * 求和
+    * @param args   长度可变的int列表
+    * @return       参数求和值
+    */
+  def sum(args: Int*) = {
+    var result = 0
+    for (arg <- args) result += arg
+    result
+  }
+
+  /**
+    * 求和
+    * @param args   长度可变的int列表
+    * @return       参数求和值
+    */
+  def recursiveSum(args: Int*): Int = {
+    if (args.length == 0) 0
+    else args.head + recursiveSum(args.tail: _*)
+  }
 
 }
--- src/main/com/hello/HelloScala.scala
+++ src/main/com/hello/HelloScala.scala
@@ -7,5 +7,22 @@ object HelloScala {
     */
   def main(args: Array[String]): Unit = {
     println(" Hello Scala!")
+    println("abs1=" + Fun.abs(100))
+    println("fac1=" + Fun.fac(1))
+    println("fac2=" + Fun.fac(2))
+    println("fac3=" + Fun.fac(3))
+    println("fac4=" + Fun.fac(4))
+    println("fac5=" + Fun.fac(5))
+    println("decorate1=" + Fun.decorate("Hello"))
+    println("decorate2=" + Fun.decorate("Hello", " --- ", " --- "))
+    println("decorate3=" + Fun.decorate(left = "<<<<<", str = "Hello", right = ">>>>>"))
+    println("sum1=" + Fun.sum(1, 2, 3, 4, 5))
+    println("sum2=" + Fun.sum(1 to 5: _*))
+    println("sum3=" + Fun.sum(1 to 100: _*))
+    println("recursiveSum1=" + Fun.recursiveSum(1))
+    println("recursiveSum2=" + Fun.recursiveSum(1, 2, 3, 4, 5))
+    println("recursiveSum3=" + Fun.recursiveSum(1 to 100: _*))
   }
+
+
 }

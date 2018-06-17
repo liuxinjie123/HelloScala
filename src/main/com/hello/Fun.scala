c4b9e22c73bc98126dc4116cea8b5ad87685454c
liuxinjie123 <jackcodejs@gmail.com>
2018-06-17 21:15:22 +0800

    scala

---
+++ src/main/com/hello/Fun.scala
@@ -0,0 +1,12 @@
+package main.com.hello
+
+object Fun {
+  /**
+    * 取绝对值
+    * @param x
+    * @return |x|
+    */
+  def abs(x: Double) = if (x > 0) x else -x;
+
+
+}
---
+++ src/main/com/hello/HelloScala.scala
@@ -0,0 +1,11 @@
+package main.com.hello
+
+object HelloScala {
+  /**
+    * 这是我的第一个scala程序
+    * 下面程序输出 Hello Scala
+    */
+  def main(args: Array[String]): Unit = {
+    println(" Hello Scala!")
+  }
+}
